77414c28b988504979f2eb9e2b55efc4d74994c9
KP from Infinitie Impex <vision.jamnagar.india@gmail.com>
2025-02-06 12:11:35 +0530

    created functions

--- main.py
+++ main.py
@@ -21,5 +21,52 @@ argument_parser.add_argument('-g', '--gallery-selector', help='Product image gal
 argument_parser.add_argument('-q', '--image-selector', help='Product image selector within "--gallery-selector" on a product page', required=True)
 argument_parser.add_argument('-o', '--output-dir', help='Product image selector within "--gallery-selector" on a product page', default=current_dir)
 
-
 args = argument_parser.parse_args()
+
+page_query_param = args.query
+
+def get_soup(link):
+    res = get(link)
+    soup = BeautifulSoup(res.content)
+    return soup
+
+def write_err_log(file_path, page_link, err, product_name=None, product_link=None):
+    file_path_length = len(file_path)
+    pattern_symbol = '='
+    half_pattern = pattern_symbol * (file_path_length / 2)
+    pattern = pattern_symbol * file_path_length
+
+    with open(file_path, 'w') as file:
+        file.write(pattern + '\n')
+        file.write(half_pattern + page_link + half_pattern + '\n')
+
+        if product_name:
+            file.write(half_pattern + product_name + half_pattern)
+
+        if product_link:
+            file.write(f'Product Link: {product_link}\n')
+
+        file.write(str(err) + '\n')
+
+        if not product_link:
+            file.write('Above error occurred on page index level\n')
+
+        file.write(pattern + '\n\n')
+
+PAGE_SLEEP_BETWEEN_REQUESTS = 3
+PRODUCT_SLEEP_BETWEEN_REQUESTS = 3
+IMAGE_SLEEP_BETWEEN_REQUESTS = 1
+
+def wait_for_page():
+    sleep(PAGE_SLEEP_BETWEEN_REQUESTS)
+
+def wait_for_product():
+    sleep(PRODUCT_SLEEP_BETWEEN_REQUESTS)
+
+def wait_for_image():
+    sleep(IMAGE_SLEEP_BETWEEN_REQUESTS)
+
+def get_element_from_selector(selector, soup):
+    element = soup.select(selector)[0]
+    return element
+
